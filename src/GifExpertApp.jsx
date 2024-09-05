00fd97746246f28092e5c4d8f93ca9c24b9ebd29
cesar0987 <cesarbenitez46@gmail.com>
2024-09-04 21:26:56 -0400

    Hooks

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  console.log(categories);
+
   return (
     <div>
+      {/*Titulo*/}
       <h2>GiftExpertApp</h2>
+      {/*Input*/}
+
+      {/*Listado de Cateogorias*/}
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </div>
   );
 };
